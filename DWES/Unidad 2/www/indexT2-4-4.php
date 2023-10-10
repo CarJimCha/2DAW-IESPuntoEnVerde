acd135582a3792aa0120794dba16f8b4b09e52ec
CarJimCha <carlosjimcha@gmail.com>
2023-10-10 21:37:47 +0200

    Trabajo del día de hoy. Faltan unos ejercicios de DWES por acabar.

--- DWES/Unidad 2/www/indexT2-4-4.php
+++ DWES/Unidad 2/www/indexT2-4-4.php
@@ -6,10 +6,24 @@
     <title>Área de un triángulo</title>
 </head>
 <body>
+    
     <h1>Ejercicio 4</h1>
 
+    <h2>CALCULAR ÁREA TRIÁNGULO</h2>
+
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+
+        <label for="cifra">Escribe la base: </label>        
+        <input type="number" id="base" name="base" value="0"><br>
+
+        <label for="cifra">Escribe la altura: </label>        
+        <input type="number" id="altura" name="altura"><br>
+
+        <input type="submit" name="calcArea" value="Calcular el área"><br>
+
+    </form>
     <?php
-    
+
     ?>
     
 </body>
